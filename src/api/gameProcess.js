a6273870772ca4e18680fcf40c956128872d5929
Dzeko <vivala0519@gmail.com>
2024-02-02 20:52:45 +0900

    [feat] 투수 타자 1대1 대결 알고리즘

--- src/api/gameProcess.js
+++ src/api/gameProcess.js
@@ -14,6 +14,7 @@ const obaGeneration = async () => {
     const valuesArray = Array.from(wOBA_Map.values())
     const totalValue = valuesArray.reduce((acc, val) => acc + val, 0)
     window.avgOBA = Math.round(totalValue / wOBA_Map.size * 1000) / 1000
+    window.ozByAvgOBA = window.avgOBA / (1 - window.avgOBA)
 }
 
 
@@ -64,13 +65,13 @@ const choiceByWeight = (options, weights) => {
 }
 
 // 타자 데이터에 따른 hit result 도출 func.
-const extractHitResult = ( batter ) => {
+const extractRunResult = ( batter ) => {
     const {one_hit, double_hit, triple_hit, home_run, BB} = batter
     const hit = Number(one_hit) + Number(double_hit) + Number(triple_hit) + Number(home_run) + Number(BB);
 
     const hitKind = ['안타', '2루타', '3루타', '홈런', '볼넷'];
     const runRate = [one_hit / hit, double_hit / hit, triple_hit / hit, home_run / hit, BB / hit];
-    console.log(runRate)
+    // console.log(runRate)
 
     const runResult = hitKind[choiceByWeight(hitKind, runRate)];
     console.log(runResult)
@@ -82,15 +83,36 @@ const extractOutResult = () => {
     const outRate = [0.33333334, 0.33333333, 0.3333333];
 
     const outResult = outKind[choiceByWeight(outKind, outRate)];
+    console.log(outResult);
     return outResult;
 }
 
 // 투수 vs 타자 대결 func.
 const singleCombat = (batter, pitcher) => {
     console.log(batter, pitcher)
-    extractHitResult(batter)
-    extractOutResult()
-    return 'out'
+    // 일 대 일 결과 도출을 위해 필요한 데이터
+    const batterObp = Number(batter.obp)
+    const pitcherObp = Number(pitcher.obp)
+    const batterLeagueObp = window.wOBAdata[batter.year] // 타자의 해당 연도 리그 평균 obp
+    const pitcherLeagueObp = window.wOBAdata[pitcher.year] // 투수의 해당 연도 리그 평균 obp
+
+    const batterOz = batterObp / (1 - batterObp)
+    const pitcherOz = (1 - pitcherObp) / pitcherObp
+    const batterLeagueObpOz = batterLeagueObp / (1 - batterLeagueObp)
+    const pitcherLeagueObpOz = (1 - pitcherLeagueObp) / pitcherLeagueObp
+    const vsOz = ((batterOz / batterLeagueObpOz) / (pitcherOz / pitcherLeagueObpOz)) * window.ozByAvgOBA
+    const vsObp = Math.round((vsOz / (1 + vsOz)) * 1000) / 1000
+
+    const hitOrOut = ['출루', '아웃']
+    const hitRate = [vsObp, 1 - vsObp]
+    const vsResult = hitOrOut[choiceByWeight(hitOrOut, hitRate)]
+
+    // 출루면 타자의 데이터로 안타 종류 추출, 아웃이면 동일 확률로 아웃 종류 추출
+    if (vsResult === '출루') {
+        extractRunResult(batter)
+    } else {
+        extractOutResult()
+    }
 }
 
 // game 진행 func.
